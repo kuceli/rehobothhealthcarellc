3ff94d43851d109e6354dbaa7ba3a8f481061bff
Englama Kuceli <englamakuceli@Kucelis-MBP.fios-router.home>
2024-08-09 15:04:51 -0400

    added shadow to headed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Logo3 from "../assets/4.png";
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between px-12 items-center font-[montserrat] py-1.5 text-sm font-semibold text-dark-blue">
+      <div className="flex justify-between px-12 items-center font-[montserrat] py-1.5 text-sm font-semibold text-dark-blue shadow">
         <img src={Logo3} className="h-11 cursor-pointer" />
 
         <ul className="flex gap-7 cursor-pointer">
