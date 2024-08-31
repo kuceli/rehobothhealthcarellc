a7cc8f9e7a1b8af24534bc81f087056b11bbc109
Englama Kuceli <englamakuceli@Kucelis-MBP.fios-router.home>
2024-08-31 16:29:37 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className="bg-white">
+    <div
+      className={`bg-white transition-opacity duration-1000 ease-in-out ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}
+    >
       <LandingPage />
     </div>
   );
